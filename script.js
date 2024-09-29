2124c12e54ede8b585aa96d7bfb8fdb140cec18d
SuperAmoledd <vothanhngu00@gmail.com>
2024-09-29 17:47:36 +0700

    chao

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 // JavaScript sẽ được sử dụng sau này nếu cần
 console.log("Trang web đã được tải thành công!");
+
+
+// Hiển thị thông báo chào mừng khi trang được tải
+window.onload = function() {
+    alert("Chào mừng bạn đến với trang web tin tức!");
+};
